297d401ac96340b9a32c687387abd336ce696ad8
Iddmeister <iddonagenda.t@gmail.com>
2024-07-07 16:09:08 +0100

    added fs

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const path = require("path")
 const crypto = require("crypto")
 const ws = require("ws")
+const fs = require("fs")
 const words = require("./words")
 
 const dotenv = require('dotenv')
