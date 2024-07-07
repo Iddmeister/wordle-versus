7b7b4e9edbfed45aeb3529f6b54968cfa8b407d9
Iddmeister <iddonagenda.t@gmail.com>
2024-07-07 15:57:19 +0100

    dunno tbh

--- client/client.js
+++ client/client.js
@@ -80,7 +80,7 @@ function switchView(newView) {
     $(newView).show()
 }
 
-var socket = new WebSocket("ws://"+location.host)
+var socket = new WebSocket("ws://"+"misc.gamesbutnogames.com:5072")
 
 socket.sendData = (data) => {
     socket.send(JSON.stringify(data))
